49c9a4e1fd3c29e6fdfdec1a81011a7de0e265f1
Abu Bakar <abubakar@karmagroup.com>
2022-07-22 06:43:24 +0800

    apis _test conn

--- config/conn.js
+++ config/conn.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 module.exports.stablishedConnection = ()=>{
 return new Promise((resolve,reject)=>{
   const con = mysql.createConnection( {
-    host: process.env.DB_HOST||localhost,
-    user: process.env.DB_USER_NAME||piment ,
-    password: process.env.DB_PASSWORD||`1Gw02wJYPM6@`,
-    database: process.env.DB_NAME||piment
+    host: localhost,
+    user: piment ,
+    password: `1Gw02wJYPM6@`,
+    database: piment
   });
   con.connect((err) => {
     if(err){
