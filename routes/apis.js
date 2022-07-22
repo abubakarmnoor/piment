2892fb944cd1dc41ebfe531a56212a6c76014c4e
Abu Bakar <abubakar@karmagroup.com>
2022-07-23 06:05:54 +0800

    apis getdata

--- routes/apis.js
+++ routes/apis.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const tsqlPull = require('../tsql/pull');
+// const tsqlPull = require('../tsql/pull');
 //fs = require('fs');
 const axios = require('axios');
-// const _api_url_default = 'http://localhost:3000'
-const _data_countries = require("../public/data/countries.json");
-const _data_pop_activity = require("../public/data/pop-activity.json");
-const _data_pop_product_family = require("../public/data/pop-product-family.json");
-const _data_pop_kayu = require("../public/data/pop-kayu.json");
-const _data_pop_unit = require("../public/data/pop-unit.json");
-const _data_pop_creator = require("../public/data/pop-creator.json");
-const _data_pop_origin = require("../public/data/pop-origin.json");
-const _data_rm = require("../public/data/raw-materials.json");
-const _data_purchase = require("../public/data/purchase.json");
+const {stablishedConnection,closeDbConnection}  =require('../config/conn');
 
 router.use(function (req, res, next) {
 	res.set('Cache-Control', 'max-age=0');// 60s x 60m x24 x ? day
@@ -21,30 +12,33 @@ router.use(function (req, res, next) {
 })
 //API - DB
 // /* GET programming languages. */
-router.get('/getdata', async function(req, res, next) {
-	try {
-		console.log(req.query.tbl);
-		console.log(req.query.page);//
-	  res.json(await tsqlPull._getdata(req.query.tbl, req.query.page));
-	} catch (err) {
-	  console.error(`Error while getting programming languages `, err.message);
-	  next(err);
-	}
-});
-const {stablishedConnection,closeDbConnection}  =require('../config/conn');
+// router.get('/getdata', async function(req, res, next) {
+// 	try {
+// 		console.log(req.query.tbl);
+// 		console.log(req.query.page);//
+// 	  res.json(await tsqlPull._getdata(req.query.tbl, req.query.page));
+// 	} catch (err) {
+// 	  console.error(`Error while getting programming languages `, err.message);
+// 	  next(err);
+// 	}
+// });
 
-router.get('/test',function(req,res){
+router.get('/getdata',function(req,res){
 	// res.status(200).json({sucess:false});
+  const _tbl = req.query.tbl
+  const _offset = req.query.offset 
+  const _page = req.query.page 
+
   stablishedConnection()
   .then((db)=>{
-    console.log("Db connection stablished");
-    db.query(`select * from tbl_rm`, null, function (err,data) { 
+    // console.log("Db connection stablished");
+    db.query(`select * from ? limit ?,?`,[_tbl, _offset, _page], null, function (err,data) { 
       if (!data) {
         res.status(200).json({sucess:false,err});
       }else{
         res.status(200).json({sucess:true,data});
         closeDbConnection(db);
-        console.log("Db Connection close Successfully");
+        // console.log("Db Connection close Successfully");
       }
   })                         
 }).catch((error)=>{
