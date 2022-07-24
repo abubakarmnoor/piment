5cfbf62e31f70d4bc4e32b7ed53cbbc87df0f389
Abu Bakar <abubakar@karmagroup.com>
2022-07-24 21:20:46 +0800

    apis pull

--- routes/apis.js
+++ routes/apis.js
@@ -34,7 +34,7 @@ router.get('/pull/:tblname',function(req,res){
       if (!data) {
         res.status(200).json({success:false,err});
       }else{
-        res.status(200).json({success:true,data});
+        res.status(200).json({success:true,data[0]});
         closeDbConnection(db);
         // console.log("Db Connection close Successfully");
       }
