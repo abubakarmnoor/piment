03c77e21fd11aa8ecee566e67ed80dd9605a3d1a
Abu Bakar <abubakar@karmagroup.com>
2022-07-24 13:41:02 +0800

    apis getdata

--- routes/apis.js
+++ routes/apis.js
@@ -25,8 +25,9 @@ router.use(function (req, res, next) {
 
 router.get('/getdata/:tblname/:offset/:page',function(req,res){
 	// res.status(200).json({sucess:false});
+  let _tbl;
     if (req.params.tblname == 'rm'){
-      const _tbl = 'tbl_rm'
+      _tbl = 'tbl_rm'
     }
 
   const _offset = req.params.offset
