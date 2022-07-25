1483fc71d4a1630a8cb5625279fdbb91933e7c07
Abu Bakar <abubakar@karmagroup.com>
2022-07-26 07:00:35 +0800

    get rm

--- public/dist/js/raw-materials.js
+++ public/dist/js/raw-materials.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
             },
             { "data": "rm_desc" },
             { "data": "rm_prod_family" },
-            {"data": "rm_cost", render: $.fn.dataTable.render.number(',', '.', 2, '')},
+            { "data": "rm_cost", render: $.fn.dataTable.render.number(',', '.', 2, '')},
             { "data": "rm_unit" },
             { "data": "rm_box_size_w" },
             { "data": "rm_box_size_l" },
