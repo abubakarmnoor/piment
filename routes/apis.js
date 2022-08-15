5ef72eabb49dd515191eebae55e5a7abcc0b8a6e
Abu Bakar <abubakar@karmagroup.com>
2022-08-16 00:30:37 +0800

    fp-details submit

--- routes/apis.js
+++ routes/apis.js
@@ -59,7 +59,7 @@ router.get('/pull/:tblname/:id?', function(req,res){
 
 router.post('/upd',(req,res)=>{
   const _data = req.body;
-  res.status(200).json({success:false, _data})
+  return res.status(200).json({success:false, _data})
 
   let query='';
   if (_data.tblname == 'rm'){
