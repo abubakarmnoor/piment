831ab8201543cb445d045a8b71eb1791f44efa7d
Abu Bakar <abubakar@karmagroup.com>
2022-08-25 06:54:39 +0800

    apis - axios

--- routes/web.js
+++ routes/web.js
@@ -9,7 +9,7 @@ const axios = require('axios').default;
 // const _api_url_default = 'http://localhost:3000'
 const _data_countries = require("../public/data/countries.json");
 const _data_pop_activity = require("../public/data/pop-activity.json");
-//const _data_pop_product_family = require("../public/data/pop-product-family.json");
+const _data_pop_product_family = require("../public/data/pop-product-family.json");
 
 const _data_pop_kayu = require("../public/data/pop-kayu.json");
 const _data_pop_unit = require("../public/data/pop-unit.json");
