e0579efc2a53a23b2e9252df0d8962197eb27403
Abu Bakar <abubakar@karmagroup.com>
2022-08-25 06:47:22 +0800

    apis - axios

--- routes/web.js
+++ routes/web.js
@@ -129,11 +129,11 @@ router.get('/raw-materials', (req, res) => {
 		tables_bs4: true, raw_materials:true
 	});
 });
-router.get('/raw-material-details/:rmid/:act', (req, res) => {
+router.get('/raw-material-details/:rmid/:act', async, (req, res) => {
 	const _act = req.params.act
 	const _rm_guid = req.params.rmid
 	// const __data_pop_product_family = _data_pop_product_family.data;
-	const __data_pop_product_family = axios.get('/apis/pop/product-family');
+	const __data_pop_product_family = await axios.get('/apis/pop/product-family');
 	const __data_pop_kayu = _data_pop_kayu.data;
 	const __data_pop_unit = _data_pop_unit.data;
 	const __data_pop_creator = _data_pop_creator.data;
