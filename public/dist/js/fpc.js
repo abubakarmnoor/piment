83f74c29bc947600cfc58c930110541416bb2b47
Abu Bakar <abubakar@karmagroup.com>
2022-09-04 10:02:16 +0800

    popup - numeric

--- public/dist/js/fpc.js
+++ public/dist/js/fpc.js
@@ -225,6 +225,23 @@ $(document).ready(function() {
         $("#fp_cp_type").val("aus");
     })
    
+    //number
+    $(".numeric").on('keyup keypress focusout', function(e){
+        
+        isNumber(e)
+        let _amt = $(this).val().replace(/,/g,"");
+        // console.log(_amt);
+        $(this).val(numberWithCommas(_amt));
+        
+    })
+    $(".numeric").on('focus', function(e){
+            
+        isNumber(e)
+        let _amt = $(this).val().replace(/,/g,"");
+        // console.log(_amt);
+        $(this).val(_amt);
+        
+    })
 //end doc ready
 });
 
