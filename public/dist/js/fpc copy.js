f0cdaaa247cbf0edd6f9818039ea0153e0862461
Abu Bakar <abubakar@karmagroup.com>
2022-09-04 17:14:23 +0800

    apis fp_cp delete

--- public/dist/js/fpc copy.js
+++ public/dist/js/fpc copy.js
@@ -77,8 +77,111 @@ $(document).ready(function() {
         tableJapan.ajax.url("/apis/pull/fp_cp/"+fp_guid+"/japan", null, false).load();
         tableUK.ajax.url("/apis/pull/fp_cp/"+fp_guid+"/uk", null, false).load();
         tableAUS.ajax.url("/apis/pull/fp_cp/"+fp_guid+"/aus", null, false).load();
+        // spinner_popup();
+        // var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+        // var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+        // if (moment(sdate_)._isValid == false){
+        //     sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+        //     edate_ = moment().format('YYYY-MM-DD');;
+        // }
+        // if (_active_tab == "lampshade"){
+        //     table.ajax.url("/data/fpc-lampshade.json", null, false).load();
+        // }else if (_active_tab == "stand"){
+        //     table.ajax.url("/data/fpc-stand.json", null, false).load();
+        // }else if (_active_tab == "euro"){
+        //     table.ajax.url("/data/fpc-euro.json"_, null, false).load();
+        // }else if (_active_tab == "us"){
+        //     table.ajax.url("/data/fpc-us.json", null, false).load();
+        // }else if (_active_tab == "japan"){
+        //     table.ajax.url("/data/fpc-japan.json", null, false).load();
+        // }else if (_active_tab == "uk"){
+        //     table.ajax.url("/data/fpc-uk.json", null, false).load();
+        // }else{
+        //     table.ajax.url("/data/fpc-aus.json", null, false).load();
+        // }
         
     })
+    //// btn tab 
+    // var _active_tab="";
+    // $("#btn_tab_lampshade").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "lampshade";
+    //     table.ajax.url("/data/fpc-lampshade.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+    // $("#btn_tab_stand").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "stand";
+    //     table.ajax.url("/data/fpc-stand.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+    // $("#btn_tab_euro").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "euro";
+    //     table.ajax.url("/data/fpc-euro.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+    // $("#btn_tab_us").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "us";
+    //     table.ajax.url("/data/fpc-us.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+    // $("#btn_tab_japan").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "japan";
+    //     table.ajax.url("/data/fpc-japan.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+    // $("#btn_tab_uk").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "uk";
+    //     table.ajax.url("/data/fpc-uk.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+    // $("#btn_tab_aus").on("click", function(){
+    //     // spinner_popup();
+    //     var sdate_ =  moment($("input[name='daterangepicker_start']").val()).format('YYYY-MM-DD');
+    //     var edate_ =  moment($("input[name='daterangepicker_end']").val()).format('YYYY-MM-DD');
+    //     if (moment(sdate_)._isValid == false){
+    //         sdate_ = moment().subtract(29, 'days').format('YYYY-MM-DD');
+    //         edate_ = moment().format('YYYY-MM-DD');;
+    //     }
+    //     _active_tab = "aus";
+    //     table.ajax.url("/data/fpc-aus.json?sdate="+sdate_+"?edate="+edate_, null, false).load();
+    // })
+
+    //// end btn tab
     
     //back
     $("#btn_back").on("click", function(e){
@@ -212,11 +315,10 @@ $(document).ready(function() {
         e.preventDefault();
         let _data = {};
         _data.id = table.row( this ).data().fp_cp_guid;
-        _data.rm_desc = table.row( this ).data().rm_desc;
         
         Swal.fire({
             title: 'Are you sure?',
-            text: "You won't be able to revert this! ("+_data.rm_desc+")",
+            text: "You won't be able to revert this!",
             icon: 'warning',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
@@ -236,7 +338,7 @@ $(document).ready(function() {
                             $('.modal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
-                                title: 'Lampshade',
+                                title: '',
                                 text: "Data Deleted"
                             }).then(function(){
                                 $("#btn_refresh").click();
