e30dc5fde8b29d1db0adbc24c52d8523df5bf3cc
Abu Bakar <abubakar@karmagroup.com>
2022-09-13 23:04:54 +0800

    rm save

--- public/dist/js/raw-material-details.js
+++ public/dist/js/raw-material-details.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
         const form = $(e.target);
         const _data = convertFormToJSON(form);
         _data.active = $("#ck_active").prop('checked')
-        _data.cost = (_data.cost).replace(/\,/g,'');//.toFixed(2);
+        _data.cost = (_data.rm_cost).replace(/\,/g,'');//.toFixed(2);
         // _data.cost = parseFloat(_data.cost).toFixed();
         _data.rm_code = $("input[name=rm_code]").val()
         _data.rm_upd_by='Admin'
