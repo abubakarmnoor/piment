77b52c1924fced98c320eefc9482c9f1056a0c9f
Abu Bakar <abubakar@karmagroup.com>
2022-09-13 23:05:21 +0800

    rm save

--- routes/apis.js
+++ routes/apis.js
@@ -101,7 +101,7 @@ router.post('/upd',(req,res)=>{
     stablishedConnection()
     .then((db)=>{
       if (_data.tblname == 'rm'){
-        query='call spsave_rm (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
+        query='call spsave_rm (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
         db.query(` `+query+` `,[_data.rm_guid, _data.rm_code, _data.rm_desc, _data.rm_prod_family, _data.rm_cost, _data.rm_unit, _data.rm_box_size_l, _data.rm_box_size_w, _data.rm_box_size_h, _data.rm_kayu, _data.rm_active, _data.updated_by], (err, data_)=>{
           if (!data_){
             res.status(200).json({success:false, err})
