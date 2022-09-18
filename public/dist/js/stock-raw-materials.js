0d206442f63566addaf24da0255ea02d65e3f5e5
Abu Bakar <abubakar@karmagroup.com>
2022-09-18 10:44:02 +0800

    stock rm - pop

--- public/dist/js/stock-raw-materials.js
+++ public/dist/js/stock-raw-materials.js
@@ -84,7 +84,7 @@ $(document).ready(function() {
     // New record
     $('a.editor-create').on('click', function (e) {
         e.preventDefault();
-        load_data_dt('/apis/pop/product-family',''); //init
+        // load_data_dt('/apis/pop/product-family',''); //init
         $("#pop-modal-form").modal('show')
     } );
 
@@ -92,7 +92,7 @@ $(document).ready(function() {
     $('#dtTbl').on('click', 'td.editor-edit', function (e) {
         e.preventDefault();
         const id = table.row( this ).data().stock_guid;
-        load_data_dt('/apis/pop/product-family',id); //init
+        // load_data_dt('/apis/pop/product-family',id); //init
         $("#pop-modal-form").modal('show')
     } );
     
@@ -100,7 +100,7 @@ $(document).ready(function() {
     $('#dtTbl').on('click', 'td.editor-details', function (e) {
         e.preventDefault();
         const id = table.row( this ).data().stock_guid;
-        load_data_dt('/apis/pop/product-family',id); //init
+        //load_data_dt('/apis/pop/product-family',id); //init
         $("#pop-modal-form").modal('show')
     } );
     // Delete a record
