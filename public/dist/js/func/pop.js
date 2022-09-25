cdd8d454704ea5260fe560f718e121185aa13c27
Abu Bakar <abubakar@karmagroup.com>
2022-09-25 13:06:56 +0800

    client order - popup

--- public/dist/js/func/pop.js
+++ public/dist/js/func/pop.js
@@ -281,6 +281,8 @@ function selectpicker_reload(id_){
         id__ = 'sp_activity';//
     }else if (id_ == 'position'){
         id__ = 'sp_position';//
+    }else if (id_ == 'co_status'){
+        id__ = 'co_status';//
     }
     // console.log(id__);
 
