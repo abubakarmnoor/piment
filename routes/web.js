82145dc5a65baff5bcd4b0c85ea305bfc71c8f19
Abu Bakar <abubakar@karmagroup.com>
2022-09-25 23:42:21 +0800

    validated

--- routes/web.js
+++ routes/web.js
@@ -278,7 +278,8 @@ router.post('/auth', validated, (req, res) => {
 		// load does not happen before session is saved
 		req.session.save(function (err) {
 		  if (err) return next(err)
-		  res.redirect('/')
+		//   res.redirect('/')
+			res.json({"success": true})
 		})
 	  })
 
