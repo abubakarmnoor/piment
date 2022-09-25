b3c83c86b50044b33767a4b02a1949f4ada2fa1b
Abu Bakar <abubakar@karmagroup.com>
2022-09-25 22:40:45 +0800

    validated

--- public/dist/js/login.js
+++ public/dist/js/login.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-    
+    $("#btn_login").on("click", function(e){
+        let _data = {}
+        _data.email = $("input[name=email").val()
+        _data.pass = $("input[name=password").val()
+        
+        spinner_popup();
+        $.ajax({
+            type:"POST", // must be POST 
+            url: "/auth", 
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify(_data),
+            success: function(data) {
+                alert(data)
+            }, 
+            error: function(jqXHR, textStatus, errorThrown) {
+                //alert(jqXHR.status);
+                $('.modal').modal('hide');
+                Swal.fire({
+                    title: "Error!",
+                    text: textStatus,
+                    icon: "error"
+                });
+            }
+        });
+
+
+    })
 })
