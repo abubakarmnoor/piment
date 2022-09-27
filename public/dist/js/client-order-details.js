68b739e49cb0bf8ecc037297621d134a48355258
Abu Bakar <abubakar@karmagroup.com>
2022-09-28 01:17:54 +0800

    co - edit

--- public/dist/js/client-order-details.js
+++ public/dist/js/client-order-details.js
@@ -114,7 +114,7 @@ function default_edit(data){
     $("input[name=co_guid").val(data[0].co_guid)
     $("input[name=co_order_id").val(data[0].co_order_id)
     $('#co_client_guid').selectpicker('val',data[0].co_client_guid)
-    const _odate = moment(new Date(data[0].co_order_date)).format("yyyy-MM-dd")
+    const _odate = moment(data[0].co_order_date).format("L")
     console.log(_odate);
     $("#co_order_date").val(_odate)
     $("#co_delivery_date").val(data[0].co_delivery_date)
