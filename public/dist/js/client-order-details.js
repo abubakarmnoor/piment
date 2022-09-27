611340542492914242dbf34875fbc23948a268be
Abu Bakar <abubakar@karmagroup.com>
2022-09-28 01:05:29 +0800

    co - edit

--- public/dist/js/client-order-details.js
+++ public/dist/js/client-order-details.js
@@ -116,8 +116,8 @@ function default_edit(data){
     $("input[name=co_guid").val(data[0].co_guid)
     $("input[name=co_order_id").val(data[0].co_order_id)
     $('#co_client_guid').selectpicker('val',data[0].co_client_guid)
-    $("input[name=co_order_date").datepicker("setDate", order_date)
-    $("input[name=co_delivery_date").datepicker("setDate", data[0].co_delivery_date)
+    $("#co_order_date").datepicker("setDate", order_date)
+    $("#co_delivery_date").datepicker("setDate", data[0].co_delivery_date)
     $('#co_status').selectpicker('val',data[0].co_status)
 
 }
