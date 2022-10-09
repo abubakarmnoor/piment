9b14b526ece6a6088aab41f48275ed20b229c946
Abu Bakar <abubakar@karmagroup.com>
2022-10-09 19:30:59 +0800

    prod - input

--- public/dist/js/production.js
+++ public/dist/js/production.js
@@ -63,7 +63,8 @@ $(document).ready(function() {
     
     //default / init
     $('.selectpicker').selectpicker();
-    selectpicker_refresh('prod_status','/apis/pop/prod-status');
+    load_data_dt('/apis/pop/co-status'); //init
+    //selectpicker_refresh('prod_status','/apis/pop/prod-status');
     selectpicker_refresh('prod_co_guid','/apis/pull/co');
     insert_element_prod_status();
 
