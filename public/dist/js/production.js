8df79bc0b3662f2f23f45252bece2d05759a1a48
Abu Bakar <abubakar@karmagroup.com>
2022-10-09 19:06:57 +0800

    prod - input

--- public/dist/js/production.js
+++ public/dist/js/production.js
@@ -166,3 +166,9 @@ $(document).ready(function() {
 //end doc ready
 });
 
+
+//insert element
+function insert_element_status(){
+    $('<a href="#" type="button" class="pull-right" id="btn_pop_co_status" data-toggle="modal" data-target="#pop-modal-form" style="margin-right: 11px"><i class="glyphicon-plus"></i> Add New</a>').insertBefore('#form_input > div:nth-child(3) > div.col-md-8.mb-3 > div > div > div');
+}
+
